eccf8ad0f86760e7102496a3184b45098a47fca8
brass-mf <sergey.s.afonin@megafon.ru>
2019-09-04 18:41:10 +0300

    6 lesson easy start

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -4,11 +4,12 @@
 class Triangle:
     def __init__(self, coordinates):
         self.coordinates = coordinates
-	def get_full_name(self):
-        return self.name + ' ' + self.surname
 
-var_triangle = Triangle(((1,2),(3,4),(5,6)))
-print('Координаты точек:'+ Triangle.coordinates)
+    def get_square(self):
+    return (self.coordinates[0][0]*(self.coordinates[1][1]-self.coordinates[2][1])+self.coordinates[1][0]*(self.coordinates[2][1]-self.coordinates[0][1])+self.coordinates[2][0]*(self.coordinates[0][1]-self.coordinates[1][1]))/2
+
+var_triangle = Triangle([(1,2),(3,4),(5,6)])
+print('Координаты точек:'+ str(var_triangle.get_square))
 
 
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
